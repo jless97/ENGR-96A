6addbfa04c97f6c74bdd79952b77ca1128cfe56d
Jason Less <jaless1997@gmail.com>
2017-06-09 02:22:30 -0700

    Added ssl encryption, add clear buf in while loop

--- secure_implementation/main.c
+++ secure_implementation/main.c
@@ -23,6 +23,9 @@
 #include <time.h> /* time_t, struct tm, time, localtime, current_time, previous_time */
 #include <mraa/aio.h>
 #include <mraa/gpio.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <openssl/evp.h>
 
 ////////////////////////////////////////////////////////////////////////////
 /////////////////////////////// Defines ////////////////////////////////////
@@ -51,6 +54,7 @@ mraa_aio_context light3;
 mraa_gpio_context button;
 // Variables to connect to server as client
 int client_socket_fd, portno;
+SSL *sslClient;
 // Buffer to store the Group ID
 char id_buf[ID_BUFFER_SIZE];
 // Buffer to store the password
@@ -78,8 +82,12 @@ int run_flag = 1;
 ////////////////////////////////////////////////////////////////////////////
 // Print usage upon unrecognized arguments
 void print_usage(void);
+// for debugging (SSL connection)
+void error(const char *msg);
 // Signal handler (SIGINT ^C)
 void handler(int signum);
+// Initialize SSL connection
+SSL * ssl_init(void);
 // Initialize the client to the server
 void initClient(int argc, char *argv[]);
 // Initialize sensors
@@ -115,6 +123,24 @@ handler(int signum) {
     exit(EXIT_SUCCESS);
 }
 
+SSL * ssl_init(void){
+	SSL_load_error_strings();
+	OpenSSL_add_all_algorithms();
+
+	const SSL_METHOD *sslMethod = TLSv1_client_method();
+	SSL_CTX *sslContext = SSL_CTX_new(sslMethod);
+
+	if (sslContext == NULL)
+		perror("ERROR can't get SSL context\n");
+
+	
+	SSL * sslClient = SSL_new(sslContext);
+	if (sslClient == NULL)
+		perror("ERROR can't get SSL structure\n");
+
+	return sslClient;	
+}
+
 void
 initClient(int argc, char *argv[]) {
     struct sockaddr_in serv_addr;
@@ -127,6 +153,10 @@ initClient(int argc, char *argv[]) {
     
     // Convert the arguments to the appropriate data types
     portno = atoi(argv[2]);
+
+    // SSL setup
+    SSL_library_init();
+    sslClient = ssl_init();
     
     // setup the socket
     client_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -138,7 +168,7 @@ initClient(int argc, char *argv[]) {
     
     // check if the IP entered by the user is valid
     server = gethostbyname(argv[1]);
-    //server = gethostbyname("localhost");
+ 
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(EXIT_FAILURE);
@@ -152,11 +182,19 @@ initClient(int argc, char *argv[]) {
     serv_addr.sin_port = htons(portno);
     
     // try to connect to the server
+    
     if (connect(client_socket_fd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0){
         fprintf(stderr, "Error connecting to server.\n");
         exit(EXIT_FAILURE);
     }
+    if (!SSL_set_fd(sslClient, client_socket_fd))
+	    perror("ERROR can't connect SSL to socket\n");
+    if (SSL_connect(sslClient) != 1)
+	    perror("ERROR server rejected connection\n");
+
+    
 }
+
 
 void
 initSensors(void) {
@@ -178,7 +216,7 @@ initSensors(void) {
     mraa_gpio_dir(button, MRAA_GPIO_IN);
     // Register the button, so that when pressed, it will be properly serviced
     mraa_gpio_isr(button, MRAA_GPIO_EDGE_RISING, (void *)&button_handler, NULL);
-
+    
 }
 
 void
@@ -188,7 +226,7 @@ shutdownSensors(void) {
     mraa_aio_close(light1);
     mraa_aio_close(light2);
     mraa_aio_close(light3);
-
+    
 }
 
 void
@@ -242,7 +280,7 @@ userPrompt(void) {
     // Prompt user to enter the ID
     fprintf(stdout, "Light sensor currently active: %d\n", button_state);
     fprintf(stdout, "\nPress the button to select which light sensor is active.\n");
-
+    
     /* Critical section */
     // Allow for user to change the state of the button for 5 seconds
     time_t endwait;
@@ -254,7 +292,7 @@ userPrompt(void) {
     }
     // Deactivate button
     button_flag = 0;
-
+    
     fprintf(stdout, "\n===================================================================\n");
     
     int i;
@@ -262,7 +300,7 @@ userPrompt(void) {
         fprintf(stdout, "In %d seconds, please enter your 1-digit group ID.\n", i);
         sleep(1);
         if (i == 1) {
-            getPassword(id_buf);
+	    getPassword(id_buf);
             fprintf(stdout, "The Group ID entered: %s.\n", id_buf);
         }
     }
@@ -293,6 +331,7 @@ userPrompt(void) {
         
         fprintf(stdout, "In %d seconds...\n", i);
         sleep(5);
+
         getPassword(pwd_buf);
         fprintf(stdout, "You entered %s so far.\n", pwd_buf);
     }
@@ -304,47 +343,47 @@ getPassword(char *buf) {
     switch (button_state) {
         // Light sensor 0 is active (values 0 or 1)
         case 0:
-            light = mraa_aio_read(light0);
-            if (light > THRESHOLD) {
-                strcat(buf, "1");
-            }
-            else {
-                strcat(buf, "0");
-            }
-            break;
+        light = mraa_aio_read(light0);
+        if (light > THRESHOLD) {
+            strcat(buf, "1");
+        }
+        else {
+            strcat(buf, "0");
+        }
+        break;
         // Light sensor 1 is active (values 2 or 3)
         case 1:
-            light = mraa_aio_read(light1);
-            if (light > THRESHOLD) {
-                strcat(buf, "3");
-            }
-            else {
-                strcat(buf, "2");
-            }
-            break;
+        light = mraa_aio_read(light1);
+        if (light > THRESHOLD) {
+            strcat(buf, "3");
+        }
+        else {
+            strcat(buf, "2");
+        }
+        break;
         // Light sensor 2 is active (values 4 or 5)
         case 2:
-            light = mraa_aio_read(light2);
-            if (light > THRESHOLD) {
-                strcat(buf, "5");
-            }
-            else {
-                strcat(buf, "4");
-            }
-            break;
+        light = mraa_aio_read(light2);
+        if (light > THRESHOLD) {
+            strcat(buf, "5");
+        }
+        else {
+            strcat(buf, "4");
+        }
+        break;
         // Light sensor 3 is active (values 6 or 7)
         case 3:
-            light = mraa_aio_read(light3);
-            if (light > THRESHOLD) {
-                strcat(buf, "7");
-            }
-            else {
-                strcat(buf, "6");
-            }
-            break;
+        light = mraa_aio_read(light3);
+        if (light > THRESHOLD) {
+            strcat(buf, "7");
+        }
+        else {
+            strcat(buf, "6");
+        }
+        break;
         default:
-            fprintf(stderr, "Error processing buttons.\n");
-            exit(EXIT_FAILURE);
+        fprintf(stderr, "Error processing buttons.\n");
+        exit(EXIT_FAILURE);
     }
 }
 
@@ -366,7 +405,7 @@ sendToServer(char *password) {
     ssize_t nwrite, nread;
     
     // send user input to the server
-    nwrite = write(client_socket_fd, password, strlen(password));
+    nwrite = SSL_write(sslClient, password, strlen(password));
     // n contains how many bytes were received by the server
     // if n is less than 0, then there was an error
     if (nwrite < 0) {
@@ -378,7 +417,7 @@ sendToServer(char *password) {
     memset(receive_buf, 0, RECV_BUFFER_SIZE);
     
     // get the response from the server and print it to the user
-    nread = read(client_socket_fd, receive_buf, RECV_BUFFER_SIZE);
+    nread = SSL_read(sslClient, receive_buf, RECV_BUFFER_SIZE);
     if (nread < 0) {
         fprintf(stderr, "Error reading from socket.\n");
         exit(EXIT_FAILURE);
@@ -399,6 +438,7 @@ sendToServer(char *password) {
     }
     
     // clean up the file descriptors
+    SSL_free(sslClient);
     close(client_socket_fd);
 }
 
@@ -416,25 +456,24 @@ main (int argc, char *argv[]) {
     
     // Initialize the button and light sensors
     initSensors();
-    
-    // Initialize buffers that will store the Group ID and password
-    memset(id_buf, 0, ID_BUFFER_SIZE);
-    memset(pwd_buf, 0, PWD_BUFFER_SIZE);
-    
-    
+ 
     // Enter password: if success, then door opened, exit success
     // If failure, can retry a total of three times before being locked out
     // for 5 minutes
     while (retry_password_flag && (retry_count != 0)) {
+        // Initialize buffers that will store the Group ID and password
+	memset(id_buf, 0, ID_BUFFER_SIZE);
+    	memset(pwd_buf, 0, PWD_BUFFER_SIZE);
+    
         // Initiate prompt to user and prompt user for password
         userPrompt();
-    
+        
         // Format username/password buffer
         createUsernamePassword();
-    
+        
         // Send username/password combination to server
         sendToServer(send_buf);
-    
+        
         // Shutdown the button and light sensors
         shutdownSensors();
     }
@@ -446,4 +485,3 @@ main (int argc, char *argv[]) {
     // If no errors encountered, success
     return EXIT_SUCCESS;
 }
-
